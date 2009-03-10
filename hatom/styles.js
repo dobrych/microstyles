ebbf54ecf7f8aa1bee56e64c6870a581c5930ba0
dobrych <dobrych@gmail.com>
2009-03-10 04:34:20 +0200

    Made JS code look and work better

--- hatom/styles.js
+++ hatom/styles.js
@@ -1,33 +1,57 @@
-$(document).ready(function() {
-    $('body').prepend($('<div id="style_select_box"><div id="style_selector"></div></div>'));
-    // style box look
-    $('#style_select_box').css({position: 'absolute', top: 0, left: 0});
-    $('#style_selector').css({
-        background: '#E5E2AF', color: '#222', border: '1px solid #222', padding: '2px',
-        'font-size': '10px', 'font-family': 'Helvetica, Arial, sans-serif',
-        'line-height': '1.5em', 'text-transform': 'uppercase', float: 'left',
-        height: '14px'
-    });
-    // calculate styles location
-    var style_base = $('link[rel="stylesheet"]').attr('href').replace(new RegExp("[0-9a-zA-Z_\-]+\.css"), '');
-    // add your styleshit here
-    $('#style_selector').append($('<strong id="microstyle_list">switch styles:</strong>'));
-        $('#style_selector').append($('<a>monoskin</a>').attr('href', style_base + 'monoskin.css'));
-        $('#style_selector').append($('<a>cold ice</a>').attr('href', style_base + 'cold-ice.css'));
-        $('#style_selector').append($('<a>franzy</a>').attr('href', style_base + 'franzy.css'));
-        $('#style_selector').append($('<a>sushi</a>').attr('href', style_base + 'sushi.css'));
-    // end of stilesheets list
-    $('#style_selector a').css({padding: '2px', color: '#222'});
-    $('#style_selector a').click(function(){
+var Microstyles = {
+    init: function() {
+        // calculate styles location
+        this.style_base = $('link[rel="stylesheet"]').attr('href').replace(new RegExp("[0-9a-zA-Z_\-]+\.css"), '');
+        // create box
+        $('body').prepend($('<div id="style_select_box"><div id="style_selector"></div></div>'));
+        // style box look
+        $('#style_select_box').css({
+            position: 'absolute', top: 0, left: 0, padding: '2px',
+            background: '#222', color: '#f1f1f1', height: '14px',
+            'font-size': '10px', 'font-family': 'Helvetica, Arial, sans-serif',
+            'line-height': '1.5em', 'text-transform': 'uppercase'
+        });
+        $('#style_selector').css({float: 'left'});
+        $('#style_selector').append($('<strong id="microstyle_list">switch styles:</strong>'));
+    },
+    // add stylesheet to box
+    add_stylesheet: function(name, file) {
+        var el = $('<a>' + name + '</a>').attr('href', this.style_base + file);
+        el.css({padding: '2px', color: '#f1f1f1'});
+        el.click(this.switch_stylesheet);
+        $('#style_selector').append(el);
+    },
+    // change global stylesheet
+    switch_stylesheet: function() {
         $('link[rel="stylesheet"]').attr('href', this.href);
         return false;
-    });
-    var selector_width = $('#style_selector').width() + 6; // 6 = 4px padding + 2px border
-    $('#style_selector').css('margin-left', -selector_width);
-    $('#style_select_box').append($('<a id="select_style_button" href="#microstyle_list"><img src="http://somerandomdude.com/images/sanscons/document.gif"/></a>'));
-    $('#select_style_button').css({float: 'right'});
-    $('#select_style_button').click(function(){
-        $('#style_selector').animate({'margin-left': 0});
+    },
+    // animate box (hide/show on left)
+    animate_box: function() {
+        var selector_width = $('#style_selector').width() + 4; // 4px padding
+        if ($('#style_select_box').css('left') == '0px')
+            { $('#style_select_box').animate({'left': -selector_width + 'px'}); }
+        else
+            { $('#style_select_box').animate({'left': '0px'}); }
         return false;
-    });
+    },
+    setup_button: function() {
+        var selector_width = $('#style_selector').width() + 4; // 4px padding
+        $('#style_select_box').css('left', -selector_width);
+        $('#style_select_box').append($('<a id="select_style_button" href="#microstyle_list">&larr;&nbsp;styles</a>'));
+        $('#select_style_button').css({float: 'right', display: 'block', color: '#f1f1f1', 'text-decoration': 'none', margin: '0px 5px'});
+        $('#select_style_button').click(Microstyles.animate_box);
+    }
+}
+
+$(document).ready(function() {
+    Microstyles.init();
+    // add your stylesheet here
+    Microstyles.add_stylesheet('monoskin', 'monoskin.css');
+    Microstyles.add_stylesheet('cold ice', 'cold-ice.css');
+    Microstyles.add_stylesheet('franzy', 'franzy.css');
+    Microstyles.add_stylesheet('sushi', 'sushi.css');
+    // end of stylesheets list
+    // setup button control for styles box
+    Microstyles.setup_button();
 });
